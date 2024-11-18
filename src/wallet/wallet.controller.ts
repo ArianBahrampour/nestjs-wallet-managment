99e5f6c8299003c867515180ed333c819a8a8d32
Arian <dev.arianbahrampour1380@gmail.com>
2024-11-18 17:17:16 +0330

    up and running

--- src/wallet/wallet.controller.ts
+++ src/wallet/wallet.controller.ts
@@ -1,9 +1,34 @@
-import { Module } from '@nestjs/common';
-import { WalletController } from './wallet.controller';
+import { Controller, Post, Get, Body, Query } from '@nestjs/common';
 import { WalletService } from './wallet.service';
+import { TransactionsDto } from 'src/common/dto/transaction.dto';
+import { WithdrawDto } from 'src/common/dto/withdraw.dto';
+import { CreateWalletDto } from 'src/common/dto/create-wallet.dto';
 
-@Module({
-  controllers: [WalletController],
-  providers: [WalletService],
-})
-export class WalletModule {}
+@Controller('/api')
+export class WalletController {
+  constructor(private readonly walletService: WalletService) {}
+
+  @Post('/generate-api-key')
+  generateApiKey(): { apiKey: string } {
+    return { apiKey: this.walletService.generateApiKey() };
+  }
+
+  @Post('/create-wallet')
+  async createWallet(): Promise<CreateWalletDto> {
+    return await this.walletService.createWallet();
+  }
+
+  @Get('/transactions')
+  async getTransactions(@Query() params: TransactionsDto): Promise<any[]> {
+    return await this.walletService.getTransactions(params.address);
+  }
+
+  @Post('/withdraw')
+  async withdraw(@Body() body: WithdrawDto): Promise<{ txId: string }> {
+    return await this.walletService.withdraw(
+      body.privateKey,
+      body.toAddress,
+      body.amount,
+    );
+  }
+}
